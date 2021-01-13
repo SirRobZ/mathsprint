9ffe139d61b68f2badd3328d1b302e6c181e23f5
Saurabh Zarapkar <sazarapkar@gmail.com>
2021-01-13 13:38:28 -0500

    refactored countdown function

--- script.js
+++ script.js
@@ -249,25 +249,27 @@ function populateGamePage() {
 
 //Display countdown
 function countdownStart() {
-  countdown.textContent = '3';
-  setTimeout(() => {
-    countdown.textContent = '2';
+  let count = 5;
+  countdown.textContent = count;
+  const timeCountDown = setInterval(() => {
+    count--;
+    if (count === 0) {
+      countdown.textContent = 'Go!';
+    } else if (count === -1) {
+      showGamePage();
+      clearInterval(timeCountDown);
+    } else {
+      countdown.textContent = count;
+    }
   }, 1000);
-  setTimeout(() => {
-    countdown.textContent = '1';
-  }, 2000);
-  setTimeout(() => {
-    countdown.textContent = 'GO!';
-  }, 3000);
 }
 
 //Navigate from splash page to countdown page
 function showCountdown() {
   splashPage.hidden = true;
   countdownPage.hidden = false;
-  countdownStart();
   populateGamePage();
-  setTimeout(showGamePage, 4000);
+  countdownStart();
 }
 
 
